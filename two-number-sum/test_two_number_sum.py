a02421249b54842d176dd233ea2d9abc692bd6b6
Daniel Wilk <wilkdaniel03@gmail.com>
2022-06-01 21:05:08 +0200

    [two number sum] creating basic dummy test file

---
+++ two-number-sum/test_two_number_sum.py
@@ -0,0 +1,23 @@
+import unittest
+
+class TestTwoNumberSum(unittest.TestCase):
+    def test_input_empty_array_approach_one(self):
+        pass
+
+    def test_input_empty_array_approach_two(self):
+        pass
+
+    def test_input_empty_array_approach_three(self):
+        pass
+
+    def test_result_approach_one(self):
+        pass
+
+    def test_result_approach_two(self):
+        pass
+
+    def test_result_approach_three(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
